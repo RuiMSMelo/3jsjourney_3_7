c57d515ad3fb5fce8661abbb9cca7e7188fe1341
RuiMSMelo <ruimsmelo@gmail.com>
2025-04-11 15:30:45 +0100

    many things added including debug

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,9 +1,22 @@
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import * as THREE from 'three'
+import Camera from './Camera.js'
+import Renderer from './Renderer.js'
+import World from './World/World.js'
+import Resources from './Utils/Resources.js'
+import sources from './sources.js'
+import Debug from './Utils/Debug.js'
+
+let instance = null
 
 export default class Experience {
   constructor(canvas) {
-    console.log('Here starts a great experience')
+    // Singleton
+    if (instance) {
+      return instance
+    }
+    instance = this
 
     // Global access
     window.experience = this
@@ -12,8 +25,14 @@ export default class Experience {
     this.canvas = canvas
 
     // Setup
+    this.debug = new Debug()
     this.sizes = new Sizes()
     this.time = new Time()
+    this.scene = new THREE.Scene()
+    this.resources = new Resources(sources)
+    this.camera = new Camera()
+    this.renderer = new Renderer()
+    this.world = new World()
 
     // Sizes resize event
     this.sizes.on('resize', () => {
@@ -26,6 +45,13 @@ export default class Experience {
     })
   }
 
-  resize() {}
-  update() {}
+  resize() {
+    this.camera.resize()
+    this.renderer.resize()
+  }
+  update() {
+    this.camera.update()
+    this.world.update()
+    this.renderer.update()
+  }
 }
